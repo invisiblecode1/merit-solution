c18ca82156ff0be96c693a9768dcee0f6765a561
invisiblecode1 <156058280+invisiblecode1@users.noreply.github.com>
2026-01-12 12:48:32 -0700

    Update base for custom domain

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import About from './pages/about';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/price" element={<Price />} />
